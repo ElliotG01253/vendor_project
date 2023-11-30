b1d17c3dcf8c3bdbafe9313d153e62e1b612325b
Elliot Green <GRE202001253@longroad.ac.uk>
2023-11-30 16:34:55 +0000

    added user class and created groups and users_groups tables in DB

--- VendorProject/VendorProject/loginVerify.cs
+++ VendorProject/VendorProject/loginVerify.cs
@@ -9,22 +9,35 @@ namespace VendorProject
 {
     internal class loginVerify
     {
+        /// <summary>
+        /// Take the input username and password and checks they are in the Database then sets the user class
+        /// </summary>
+        /// <param name="username"></param>
+        /// <param name="password"></param>
         public void loginVerification(string username, string password)
         {
-            // to help me :https://learn.microsoft.com/en-us/dotnet/api/system.data.datatable?view=net-8.0
-            DataSet ds = DBConnection.GetDBConnection().GetDataSet("SELECT * FROM loginInfo");
+            DataSet ds = DBConnection.GetDBConnection().GetDataSet("SELECT * FROM Users");
             DataTable dt = ds.Tables[0];
             DataRow[] dr = dt.Select();
             foreach (DataRow dataRow in dr)
             {
-                if (dataRow[1].ToString() == username)
+                if (dataRow[1].ToString()!.Trim() == username)
                 {
-                    if (dataRow[2].ToString() == password)
+                    if (dataRow[2].ToString()!.Trim() == password)
                     {
                         //set a class "user" account info 
+                        setUser(username, int.Parse(dataRow[0].ToString()!));
                     }
                 }
             }
+        }
+
+        private void setUser(string username, int userId)
+        {
+            DataSet ds = DBConnection.GetDBConnection().GetDataSet($"SELECT g.name FROM groups_users gu , groups g where g.id = gu.groups_id AND gu.users_id = {userId}");
+            User user = new User();
+            user.setter(userId, ds.Tables[0].Rows[0]["group"].ToString()!);
+
         }
     }
 }
---
+++ VendorProject/VendorProject/user.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VendorProject
+{
+    internal class User
+    {
+        public int id;
+        public string? group;
+
+        public void setter(int userId, string userGroup)
+        {
+            id = userId;
+            group = userGroup;
+        }
+        public int getUserId() { return id; }
+        public string getGroup() { return group!; }
+    }
+}
