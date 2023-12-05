7f18b8dd8c094e826578ace5b51204a706d885ee
MadinShindi09 <MMS182@student.aru.ac.uk>
2023-12-06 04:38:13 +0530

    Database Added

--- VendorProject/VendorProject/DBConnection.cs
+++ VendorProject/VendorProject/DBConnection.cs
@@ -1,12 +1,6 @@
-﻿using Microsoft.VisualBasic.ApplicationServices;
-using System;
-using System.Collections.Generic;
+﻿using System;
 using System.Data;
-using System.Data.Common;
 using System.Data.SqlClient;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace VendorProject
 {
@@ -15,21 +9,13 @@ namespace VendorProject
         private static DBConnection? _instance;
         private string connectionString;
         private SqlConnection? connectionToDB;
-        /// <summary>
-        /// gets the connection string from project settings
-        /// </summary>
+
         private DBConnection()
         {
-
-            //connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C: \\Users\\eg593\\AppData\\Local\\Development\\vendor_project\\VendorProject\\VendorProject\\accountInfo.mdf";
-            connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=D:\\Vendor_group_project\\vendor_project\\VendorProject\\VendorProject\\accountInfo.mdf;Integrated Security=True";
-            //connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\accountInfo.mdf;Integrated Security=True;";
-            //connectionString = Properties.Settings.Default.DBConnectionString;
+            // Using SQL Server authentication
+            connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;Initial Catalog=Vendor_Information;User ID=yourUsername;Password=yourPassword;Connect Timeout=30";
         }
-        /// <summary>
-        /// method to check if there is allready an instance of the database connection and if there is it just returns the DB connection string
-        /// </summary>
-        /// <returns>DBConnection</returns>
+
         public static DBConnection GetDBConnection()
         {
             if (_instance == null)
@@ -38,21 +24,29 @@ namespace VendorProject
             }
             return _instance;
         }
-        /// <summary>
-        /// method creates a dataset and fills it with data determined by the sql Statement given in the method
-        /// </summary>
-        /// <param name="sqlStatement"></param>
-        /// <returns>DataSet</returns>
+
         public DataSet GetDataSet(string sqlStatement)
         {
             DataSet dataSet = new DataSet();
-            using (connectionToDB = new SqlConnection(connectionString))
+            try
             {
-                connectionToDB.Open();
-                SqlDataAdapter dataAdapter = new SqlDataAdapter(sqlStatement, connectionToDB);
-
-                dataAdapter.Fill(dataSet);
-                connectionToDB.Close();
+                using (connectionToDB = new SqlConnection(connectionString))
+                {
+                    connectionToDB.Open();
+                    SqlDataAdapter dataAdapter = new SqlDataAdapter(sqlStatement, connectionToDB);
+                    dataAdapter.Fill(dataSet);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error accessing the database: " + ex.Message);
+            }
+            finally
+            {
+                if (connectionToDB != null && connectionToDB.State == ConnectionState.Open)
+                {
+                    connectionToDB.Close();
+                }
             }
             return dataSet;
         }
--- VendorProject/VendorProject/obj/Debug/net6.0-windows/VendorProject.AssemblyInfo.cs
+++ VendorProject/VendorProject/obj/Debug/net6.0-windows/VendorProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VendorProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5301a9ca5d0deb0045e585da1b0c42353a805ffb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c57a611110548816bba84a374fd9379a33f43d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("VendorProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VendorProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
